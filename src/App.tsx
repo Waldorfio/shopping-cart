727646ac4e07f6a3361c15b30641673692851c21
Walid VM <walid.hanifi707@gmail.com>
2022-11-04 18:37:45 +1000

    Add styling for header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import './styles/style.css';
 
 function App() {
   return (
-    <div>
+    <div id='container'>
+
       <div id='header'>
-        <div id='logo'>A Real Shop</div>
-        <div id='home'>Home</div>
-        <div id='shop'>Shop</div>
-        <div id='about'>About</div>
+        <div id='header_one'>
+          <div id='logo'>A Real Shop</div>
+        </div>
+        <div id='header_two'>
+          <div id='home'>Home</div>
+          <div id='shop'>Shop</div>
+          <div id='about'>About</div>
+          <span id='cart' className="material-symbols-outlined">shopping_cart</span>
+        </div>
       </div>
-      <div></div>
 
-      <div id='content'>Content</div>
+      <div id='content'>
+        Content
+      </div>
 
-      <div id='footer'></div>  
+      <div id="footer">
+          <span id='footer-title'>Copyright © 2022 Waldorfio&nbsp;&nbsp;&nbsp;
+            <img id='git' src={require('./img/github-logo.png')} alt={'github-logo'}></img>
+          </span>
+      </div>
+
     </div>
   );
 }
