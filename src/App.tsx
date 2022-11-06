d3953463a1a3e66b76589c26991496ec24385b67
Walid VM <walid.hanifi707@gmail.com>
2022-11-06 18:07:57 +1000

    Add cart size notification number

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import './styles/style.css';
 import {Link, Outlet} from 'react-router-dom'
+import React, { useState } from 'react';
 
 function App() {
+  // State Declarations
+  const [cart, setCart] = useState(0);
+
   return (
     <div id='container'>
 
       <div id='header'>
         <div id='header_one'>
-          <Link id='logo' to='/'>A Real Shop</Link>
+          <Link id='logo' to='/'>Another Key Shop</Link>
         </div>
         <div id='header_two'>
 
           <Link id='home' to='/'>Home</Link>
           <Link id='shop' to='/shop'>Shop</Link>
           <Link id='about' to='/about'>About</Link>
-          <span id='cart' className="material-symbols-outlined">shopping_cart</span>
+          <div id='cart-container'>
+            <span id='cart' className="material-symbols-outlined">shopping_cart</span>
+            <div id='cart-size'>{cart}</div>
+          </div>
         </div>
       </div>
 
-      <Outlet />
+      <Outlet context={[cart, setCart]}/>
 
       <div id="footer">
           <span id='footer-title'>Copyright © 2022 Waldorfio&nbsp;&nbsp;&nbsp;
