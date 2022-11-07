4ab492c8e7c52fdee0812d7a865a7081481d709e
Walid VM <walid.hanifi707@gmail.com>
2022-11-07 22:01:11 +1000

    Fix home page routing

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div id='about-content'>
+        <div id='contact-info'>Home Page22</div>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './Home';
 import Shop from './Shop';
 import About from './About'
 
@@ -8,6 +9,7 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} >
+          <Route index element={<Home />} /> {/* resets the default index/homepage to the home component */}
           <Route path='shop' element={<Shop />} />
           <Route path='about' element={<About />} />
         </Route>
