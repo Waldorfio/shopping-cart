4282de832c5e315a7d0e2a1fbc93b10779c6a7d8
Walid VM <walid.hanifi707@gmail.com>
2022-11-09 21:05:35 +1000

    Add styling for homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
     return (
-      <div id='about-content'>
-        <div id='contact-info'>Home Page22</div>
+      <div id='home-content'>
+        <div id='home-text'>Best online cd key reseller of the year</div>
+        <div id='main-home-text'>Real keys.<br></br>Real prices.<br></br>No hidden fees.</div>
+        <Link id='shop-now' to='/shop'>Shop now</Link>
+        <img id='game-samples' src={require('./img/frontshop.PNG')}></img>
       </div>
     );
   };
