697b58efccbb4d815c13d1df7b7c85bf3337c746
Walid VM <walid.hanifi707@gmail.com>
2022-11-09 12:08:22 +1000

    Add HashRouter routing for gh-pages hosting

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './Home';
 import Shop from './Shop';
@@ -7,13 +7,15 @@ import About from './About'
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} >
-          <Route index element={<Home />} /> {/* resets the default index/homepage to the home component */}
-          <Route path='shop' element={<Shop />} />
-          <Route path='about' element={<About />} />
-        </Route>
-      </Routes>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path='/' element={<App />} >
+            <Route index element={<Home />} /> {/* resets the default index/homepage to the home component */}
+            <Route path='shop' element={<Shop />} />
+            <Route path='about' element={<About />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </BrowserRouter>
   );
 };
