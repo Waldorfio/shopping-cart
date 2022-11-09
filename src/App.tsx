9458b907a5133d930b9838dacea825d51b9f0aa9
Walid VM <walid.hanifi707@gmail.com>
2022-11-09 11:35:28 +1000

    Revert to previous commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,27 @@
 import './styles/style.css';
 import {Link, Outlet} from 'react-router-dom'
-import React, { SyntheticEvent, useState, useRef } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   // State Declarations
   const [cart, setCart] = useState(0);
 
-  // Declaring Refs for DOM Manipulation
-  const cartRef = useRef<HTMLDivElement>(null);
-  const cartPopupRef = useRef<HTMLDivElement>(null);
-
-  // DOM Handler Functions
-  function showCart(e: SyntheticEvent) {
-    const cartPopup = cartPopupRef.current;
-    // Show Element if clicked button, and not already visible
-    if (cartPopup!.style.display == 'none') {
-      cartPopup!.style.display = '';
-    // Else hide element if already shown, and button was clicked
-    } else {
-        cartPopup!.style.display = 'none';
-    }
-  }
-
-  function exitCart(e: SyntheticEvent) {
-    const cartPopup = cartPopupRef.current;
-    const cart = cartRef.current;
-    if (e.target !== cartPopup && e.target !== cart) {
-      cartPopup!.style.display = "none";
-    }
-  }
-
-  // State Handler
   function addToCart() {
     setCart(cart + 1);
   }
 
+  function showCart(e: any) {
+    const cartP: any = document.getElementById('cart-popup');
+    if (cartP.style.display == 'none') { // Then show element
+      cartP.style.display = '';
+      cartP.innerHTML = 'text';
+    } else { // Hide element
+        cartP.style.display = 'none';
+    }
+  }
+
   return (
-    <div id='container' onClick={exitCart}>
+    <div id='container'>
 
       <div id='header'>
         <div id='header_one'>
@@ -48,7 +33,7 @@ function App() {
           <Link id='shop' to='/shop'>Shop</Link>
           <Link id='about' to='/about'>About</Link>
           <div id='cart-container'>
-            <span onClick={showCart} ref={cartRef} id='cart' className="material-symbols-outlined">shopping_cart</span>
+            <span onClick = {showCart} id='cart' className="material-symbols-outlined">shopping_cart</span>
             <div id='cart-size'>{cart}</div>
           </div>
         </div>
@@ -57,21 +42,7 @@ function App() {
       <Outlet context={addToCart}/>
 
       <div id="cart-popup-container">
-        <div ref={cartPopupRef} id="cart-popup" style={{display: 'none'}}>
-          <div className='cart-text'>
-            MY CART
-            <span className='sub-cart'>{cart} ITEMS</span>
-            <div id='close-cart'>x</div>
-          </div>
-          <div id='cart-content'>
-            You have no items in your shopping cart.
-          </div>
-          <div id='cart-footer'>
-            <div id='subtotal' className='sub-cart'>SUBTOTAL</div>
-            <div id='total-price' className='cart-text'>AU$93.58</div>
-            <button id='checkout-btn'>CHECKOUT</button>
-          </div>
-        </div> 
+        <div id="cart-popup"></div> 
       </div>
 
       <div id="footer">
