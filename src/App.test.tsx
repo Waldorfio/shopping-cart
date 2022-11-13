8472a743827ef5fe2fa18199f5de169edb119ccf
Walid VM <walid.hanifi707@gmail.com>
2022-11-13 16:24:11 +1000

    Seperate tests into components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,44 +5,49 @@ import "@testing-library/jest-dom";
 import App from "./App";
 import Home from "./Home";
 
-test('Test the landing page', () => {
-    render(
-        <Router>
-          <App />
-        </Router>,
-      )
-    
-    expect(screen.getByText(/Another Key Shop/i)).toBeInTheDocument();
-});
+describe('App component', () => {
 
-test('Test empty cart (0 items)', () => {
-    render(
-        <Router>
-          <App />
-        </Router>,
-      )
-    
-    expect(screen.getByText('0 ITEMS')).toBeInTheDocument();
-});
-
-test('Test image (github logo)', () => {
-    render(
-        <Router>
-          <App />
-        </Router>,
-      )
-    
-    expect(screen.getByAltText('github-logo')).toHaveAttribute('src', 'github-logo.png')
-});
-
-test('Test image (game banner)', () => {
-    render(
-        <Router>
+    test('Test the landing page', () => {
+        render(
+            <Router>
             <App />
-                <Home />
-        </Router>,
-      )
-    
-    expect(screen.getByAltText('game-banner')).toHaveAttribute('src', 'frontshop.PNG')
-});
+            </Router>,
+        )
+        
+        expect(screen.getByText(/Another Key Shop/i)).toBeInTheDocument();
+    });
+    test('Test empty cart (0 items)', () => {
+        render(
+            <Router>
+            <App />
+            </Router>,
+        )
+        
+        expect(screen.getByText('0 ITEMS')).toBeInTheDocument();
+    });
+    test('Test image (github logo)', () => {
+        render(
+            <Router>
+            <App />
+            </Router>,
+        )
+        
+        expect(screen.getByAltText('github-logo')).toHaveAttribute('src', 'github-logo.png')
+    });
 
+})
+
+describe('Home component', () => {
+
+    test('Test image (game banner)', () => {
+        render(
+            <Router>
+                <App />
+                    <Home />
+            </Router>,
+        )
+        
+        expect(screen.getByAltText('game-banner')).toHaveAttribute('src', 'frontshop.PNG')
+    });
+
+})
