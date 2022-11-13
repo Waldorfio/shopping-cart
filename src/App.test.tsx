c398482e3fbdfe4b1291f605dad2612acb499c8b
Walid VM <walid.hanifi707@gmail.com>
2022-11-13 16:12:32 +1000

    Add tests for home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
+import Home from "./Home";
 
 test('Test the landing page', () => {
     render(
@@ -22,5 +23,26 @@ test('Test empty cart (0 items)', () => {
       )
     
     expect(screen.getByText('0 ITEMS')).toBeInTheDocument();
+});
+
+test('Test image (github logo)', () => {
+    render(
+        <Router>
+          <App />
+        </Router>,
+      )
+    
+    expect(screen.getByAltText('github-logo')).toHaveAttribute('src', 'github-logo.png')
+});
+
+test('Test image (game banner)', () => {
+    render(
+        <Router>
+            <App />
+                <Home />
+        </Router>,
+      )
+    
+    expect(screen.getByAltText('game-banner')).toHaveAttribute('src', 'frontshop.PNG')
 });
 
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div id='home-text'>Best online cd key reseller of the year</div>
         <div id='main-home-text'>Real keys.<br></br>Real prices.<br></br>No hidden fees.</div>
         <Link className='shop-btn' id='shop-now' to='/shop'>Shop now</Link>
-        <img id='game-samples' src={require('./img/frontshop.PNG')}></img>
+        <img id='game-samples' alt={'game-banner'} src={require('./img/frontshop.PNG')}></img>
       </div>
     );
   };
