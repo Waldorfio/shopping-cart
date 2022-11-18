169bab7e2e45ff770ed6ffd0ac01a231d4bb3ca7
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-11-18 15:51:10 +1000

    Add logos for social media buttons

--- src/About.tsx
+++ src/About.tsx
@@ -2,11 +2,11 @@ const About = () => {
     return (
       <div id='about-content'>
         <div id='about-links'>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
+          <a href="github/com/waldorfio" class="fa fa-github"></a>
+          <a href="https://www.linkedin.com/in/wally-h/" class="fa fa-linkedin"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-instagram"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-discord"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-youtube"></a>
         </div>
         <div id='contact-info'>Another Key Shop<br></br>Address not provided lol</div>
       </div>
