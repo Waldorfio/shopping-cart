0d91e41b304014ce2e0b85fc9b5e7ef2f40a6c86
Walid VM <walid.hanifi707@gmail.com>
2022-11-18 17:16:48 +1000

    Add styling for About page logos

--- src/About.tsx
+++ src/About.tsx
@@ -2,11 +2,10 @@ const About = () => {
     return (
       <div id='about-content'>
         <div id='about-links'>
-          <a href="github/com/waldorfio" class="fa fa-github"></a>
-          <a href="https://www.linkedin.com/in/wally-h/" class="fa fa-linkedin"></a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-instagram"></a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-discord"></a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa fa-youtube"></a>
+          <a href="https://www.github.com/waldorfio" id='github-logo' className="fa fa-github"></a>
+          <a href="https://www.linkedin.com/in/wally-h/" id='linked-logo' className="fa fa-linkedin"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id='insta-logo' className="fa fa-instagram"></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id='youtube-logo' className="fa fa-youtube"></a>
         </div>
         <div id='contact-info'>Another Key Shop<br></br>Address not provided lol</div>
       </div>
